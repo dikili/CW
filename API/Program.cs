332f7d9e13571d1ba169004c9e005cf6ffbdf78a
Dikili <dikili@outlook.com>
2025-02-16 23:02:43 +0000

    +1

--- API/Program.cs
+++ API/Program.cs
@@ -11,8 +11,11 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 });
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
 app.MapControllers();
 
