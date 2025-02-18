91484702ae6a426d4cf5ac39332a1773c943097c
Dikili <dikili@outlook.com>
2025-02-18 17:34:47 +0000

    backend end

---
+++ Application/Activities/Queries/GetActivityList.cs
@@ -0,0 +1,27 @@
+using System;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Activities.Queries;
+
+public class GetActivityList
+{
+    public class Query : IRequest<List<Activity>> { }
+
+    public class Handler(AppDbContext context) : IRequestHandler<Query, List<Activity>>
+    {
+        // private readonly DataContext _context;
+        // public Handler(DataContext context)
+        // {
+        //     _context = context;
+        // }
+
+        public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            return await context.Activities.ToListAsync(cancellationToken);
+        }
+    }
+
+}
---
+++ Application/Core/MapperProfiles.cs
@@ -0,0 +1,13 @@
+using System;
+using AutoMapper;
+using Domain;
+
+namespace Application.Core;
+
+public class MapperProfiles : Profile
+{
+    public MapperProfiles()
+    {
+        CreateMap<Activity, Activity>();
+    }
+}
